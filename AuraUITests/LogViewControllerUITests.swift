d5627f9294118de92bc0a2471096004277630fb5
freegatik <anton.soloviev2016@gmail.com>
2025-04-30 15:10:00 +0300

    Log smoke test: main screen elements

---
+++ AuraUITests/LogViewControllerUITests.swift
@@ -0,0 +1,39 @@
+//
+//  LogViewControllerUITests.swift
+//  AuraUITests
+//
+//  Created by Anton Solovev on 30.04.2025.
+//
+
+
+import XCTest
+@testable import Aura
+
+final class LogViewControllerUITests: XCTestCase {
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+        
+        let appleIDButton = app.otherElements["AppleIDButton"]
+        appleIDButton.tap()
+    }
+    
+    func testMainElementsExist() {
+        let navBar = app.otherElements["LogNavBar"]
+        XCTAssertTrue(navBar.exists, "Навигационная панель должна быть видна на экране")
+        
+        let titleLabel = app.staticTexts["LogTitleLabel"]
+        XCTAssertTrue(titleLabel.exists, "Заголовок должен быть виден на экране")
+        
+        let activityRing = app.otherElements["ActivityRingView"]
+        XCTAssertTrue(activityRing.exists, "Кольцо активности должно быть видно на экране")
+        
+        let addButton = app.otherElements["AddNoteButton"]
+        XCTAssertTrue(addButton.exists, "Кнопка добавления заметки должна быть видна на экране")
+        XCTAssertTrue(addButton.isHittable, "Кнопка добавления заметки должна быть доступна для нажатия")
+    }
+}
